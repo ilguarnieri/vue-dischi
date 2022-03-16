50f61703f32576f142efc9dfb370e25cd6600e1d
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-03-17 00:55:37 +0100

    add loading css

--- src/components/SpMain.vue
+++ src/components/SpMain.vue
@@ -2,7 +2,10 @@
 
     <main>   
         <div class="container">
-            <div class="albums-wrapper">
+
+            <div v-if="albums.length == 0" class="lds-ring"><div></div><div></div><div></div><div></div></div>
+
+            <div v-else class="albums-wrapper">
 
             <AlbumItems v-for="(el,i) in albums" :key="i" :album="el"/>
 
@@ -47,7 +50,7 @@ export default{
     },
 
     created() {
-        setTimeout(this.fetchAlbums, 1000)
+        setTimeout(this.fetchAlbums, 1500)
     }
 }
 
@@ -73,6 +76,42 @@ main{
 
         & > *{
             width: 200px;
+        }
+    }
+
+    .lds-ring {
+        display: inline-block;
+        position: relative;
+        width: 80px;
+        height: 80px;
+    }
+    .lds-ring div {
+        box-sizing: border-box;
+        display: block;
+        position: absolute;
+        width: 64px;
+        height: 64px;
+        margin: 8px;
+        border: 8px solid #fff;
+        border-radius: 50%;
+        animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+        border-color: #fff transparent transparent transparent;
+    }
+    .lds-ring div:nth-child(1) {
+        animation-delay: -0.45s;
+    }
+    .lds-ring div:nth-child(2) {
+        animation-delay: -0.3s;
+    }
+    .lds-ring div:nth-child(3) {
+        animation-delay: -0.15s;
+    }
+    @keyframes lds-ring {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
         }
     }
 }
